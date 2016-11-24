0933b05930db72ed5cf1e12c7c338180f413f67b
Michael Klishin <mklishin@pivotal.io>
2016-11-24 17:27:58 +0300

    Add a before_build script
    This reconfigures mgmt plugin to work better as of rabbitmq/rabbitmq-management#236.
    We do the same in other HTTP API clients.

---
+++ bin/before_build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+${HOP_RABBITMQCTL:="sudo rabbitmqctl"}
+
+# guest:guest has full access to /
+
+$HOP_RABBITMQCTL add_vhost /
+$HOP_RABBITMQCTL add_user guest guest
+$HOP_RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
+
+# Reduce retention policy for faster publishing of stats
+$HOP_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
