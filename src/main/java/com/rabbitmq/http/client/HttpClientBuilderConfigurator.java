6c97326ca830b8056daf6477d8b1fa6855ced7bb
Michael Klishin <mklishin@pivotal.io>
2017-02-25 07:02:23 +0300

    Doco

--- src/main/java/com/rabbitmq/http/client/HttpClientBuilderConfigurator.java
+++ src/main/java/com/rabbitmq/http/client/HttpClientBuilderConfigurator.java
@@ -2,6 +2,15 @@ package com.rabbitmq.http.client;
 
 import org.apache.http.impl.client.HttpClientBuilder;
 
+/**
+ * Implementations of this interface can perform post-configuration
+ * of {@link HttpClientBuilder} used by {@link Client} internally.
+ * Note that {@link Client} will configure all essential settings
+ * before invoking the configurator.
+ *
+ * @see Client#setHttpClientBuilderConfigurator(HttpClientBuilderConfigurator)
+ * @see NoOpHttpClientBuilderConfigurator
+ */
 public interface HttpClientBuilderConfigurator {
   public HttpClientBuilder configure(HttpClientBuilder builder);
 }
