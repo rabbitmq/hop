3fb733cc0dd9b3333647d25959aac957e3de4baa
Michael Klishin <michael@clojurewerkz.org>
2018-12-06 16:47:36 +0300

    before_build.sh: avoid invoking rabbitmqctl without arguments

--- bin/before_build.sh
+++ bin/before_build.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-${HOP_RABBITMQCTL:="sudo rabbitmqctl"}
-${HOP_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+CTL=${HOP_RABBITMQCTL:="sudo rabbitmqctl"}
+PLUGINS=${HOP_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
 
-$HOP_RABBITMQ_PLUGINS enable rabbitmq_management
+PLUGINS enable rabbitmq_management
 
 sleep 3
 
 # guest:guest has full access to /
 
-$HOP_RABBITMQCTL add_vhost /
-$HOP_RABBITMQCTL add_user guest guest
-$HOP_RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
-$HOP_RABBITMQCTL set_topic_permissions -p / guest amq.topic ".*" ".*"
+CTL add_vhost /
+CTL add_user guest guest
+CTL set_permissions -p / guest ".*" ".*" ".*"
+CTL set_topic_permissions -p / guest amq.topic ".*" ".*"
 
-$HOP_RABBITMQCTL add_vhost vh1
-$HOP_RABBITMQCTL set_permissions -p vh1 guest ".*" ".*" ".*"
+CTL add_vhost vh1
+CTL set_permissions -p vh1 guest ".*" ".*" ".*"
 
-$HOP_RABBITMQCTL add_vhost vh2
-$HOP_RABBITMQCTL set_permissions -p vh2 guest ".*" ".*" ".*"
+CTL add_vhost vh2
+CTL set_permissions -p vh2 guest ".*" ".*" ".*"
 
 # Reduce retention policy for faster publishing of stats
-$HOP_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
-$HOP_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_agent_sup_sup, rabbit_mgmt_agent_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_agent_sup_sup:start_child().'
+CTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
+CTL eval 'supervisor2:terminate_child(rabbit_mgmt_agent_sup_sup, rabbit_mgmt_agent_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_agent_sup_sup:start_child().'
 
 # Enable shovel plugin
-$HOP_RABBITMQ_PLUGINS enable rabbitmq_shovel
-$HOP_RABBITMQ_PLUGINS enable rabbitmq_shovel_management
+PLUGINS enable rabbitmq_shovel
+PLUGINS enable rabbitmq_shovel_management
 
 true
