10019d0d9d99de2d7d582de6e0f5db831bf8f0a6
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-07-23 15:13:16 +0200

    Bump versions in sanity check script

--- sanity-check.groovy
+++ sanity-check.groovy
@@ -1,8 +1,8 @@
 @GrabResolver(name = 'spring-staging', root = 'https://repo.spring.io/libs-staging-local/')
 @Grab(group = 'com.rabbitmq', module = 'http-client', version = "${version}")
-@Grab(group = 'org.springframework', module = 'spring-web', version = "5.1.5.RELEASE")
-@Grab(group = 'org.apache.httpcomponents', module = 'httpclient', version = "4.5.6")
-@Grab(group = 'org.slf4j', module = 'slf4j-simple', version = '1.7.25')
+@Grab(group = 'org.springframework', module = 'spring-web', version = "5.1.8.RELEASE")
+@Grab(group = 'org.apache.httpcomponents', module = 'httpclient', version = "4.5.9")
+@Grab(group = 'org.slf4j', module = 'slf4j-simple', version = '1.7.26')
 
 import com.rabbitmq.http.client.Client
 import org.slf4j.LoggerFactory
