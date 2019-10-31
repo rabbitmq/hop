b464127d4f09e8f37897b7f0bfe4d4a793f1ca82
Andrey Somov <public.somov@gmail.com>
2019-10-31 15:46:18 +0300

    Remove deprecated methods in ShovelDetails

--- src/main/java/com/rabbitmq/http/client/domain/ShovelDetails.java
+++ src/main/java/com/rabbitmq/http/client/domain/ShovelDetails.java
@@ -88,25 +88,6 @@ public class ShovelDetails {
 		this.publishProperties = publishProperties;
 	}
 
-	/**
-	 * @return the first source source URI
-	 * @deprecated use {@link #getSourceURIs()} instead
-	 */
-	@Deprecated
-	@JsonIgnore
-	public String getSourceURI() {
-		return sourceURIs.get(0);
-	}
-
-	/**
-	 * @param sourceURI the source URI
-	 * @deprecated use {@link #setSourceURIs(List)} instead
-	 */
-	@Deprecated
-	public void setSourceURI(String sourceURI) {
-		this.sourceURIs = Collections.singletonList(sourceURI);
-	}
-
 	public List<String> getSourceURIs() {
         return sourceURIs;
 	}
@@ -138,26 +119,6 @@ public class ShovelDetails {
 
 	public void setSourceQueue(String sourceQueue) {
 		this.sourceQueue = sourceQueue;
-	}
-
-	/**
-	 * @return the first destination URI
-	 * @deprecated use {@link #getDestinationURIs()} instead
-	 */
-	@Deprecated
-	@JsonIgnore
-	public String getDestinationURI() {
-		return destinationURIs.get(0);
-	}
-
-	/**
-	 *
-	 * @param destURI the destination URI
-	 * @deprecated use {@link #setDestinationURIs(List)} instead
-	 */
-	@Deprecated
-	public void setDestinationURI(String destURI) {
-        this.destinationURIs = Collections.singletonList(destURI);
 	}
 
 	public List<String> getDestinationURIs() {
