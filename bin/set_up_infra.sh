283db0de8a3c8612e4ee4298716a7ca551b53ee5
Michael Klishin <michael@clojurewerkz.org>
2021-03-24 12:23:33 +0300

    Provision 3.8.14

--- bin/set_up_infra.sh
+++ bin/set_up_infra.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.9/rabbitmq-server-generic-unix-3.8.9.tar.xz
-tar xf rabbitmq-server-generic-unix-3.8.9.tar.xz
-mv rabbitmq_server-3.8.9 rabbitmq
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.14/rabbitmq-server-generic-unix-3.8.14.tar.xz
+tar xf rabbitmq-server-generic-unix-3.8.14.tar.xz
+mv rabbitmq_server-3.8.14 rabbitmq
 
 rabbitmq/sbin/rabbitmq-server -detached
 
-sleep 10
-
+rabbitmq/sbin/rabbitmqctl await_startup
+sleep 7
 rabbitmq/sbin/rabbitmqctl status
 
 true
